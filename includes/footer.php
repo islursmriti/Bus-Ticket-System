1d605c54c106f9c05970d3e3fffa0239e52518d3
smritiislur <smritivenkatislur@gmail.com>
2022-04-28 16:45:12 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+
+	<div class="copy-right"> 
+		<div class="container">
+			<p>2019120028, 2019120029, 2019120051</p>
+			<p>© 2022 Bus Ticket Management System</p>
+		</div> 
+	</div> 
+	<!-- //footer -->   
+	
